e1975d396cea38378a8190187cc259aedf7ca4ee
Daniele Zurico <daniele.zurico@blackswan.com>
2018-08-22 16:45:25 +0100

    daniele

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar openSideMenu={openSideMenu}></NavBar>
-        <SideBar closeSideMenu={closeSideMenu}></SideBar>
+        <NavBar></NavBar>
+        <SideBar></SideBar>
 		
         <div id='main'>
           <h1>Responsive Slide Menu</h1>
